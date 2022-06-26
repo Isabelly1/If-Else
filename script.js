ecc6a9b09da73c5aefd9eca6fb8d51ba5cd1d452
Isabelly1 <isabellymartins846@gmail.com>
2022-06-26 15:31:10 -0300

    if-else

---
+++ script.js
@@ -0,0 +1,33 @@
+/*exercício 1*/
+{let idade = 19
+    if (idade>18){
+    console.log("Parabês, por essa nova etapa")
+    }else
+    {
+    console.log("Infelizmente você ainda não é de maior") 
+    }
+    }
+    /*Exercício 2 */
+    {let idade = 15
+    let humano = true
+    
+    if (idade>18 && humano){
+    console.log("Você é humano e maior de idade. EStou certa ?")
+    }else
+    {
+    console.log("Você talvez seja menor de idade")
+    }
+    }
+    
+    /*Exercício 3 */
+    {let mes = "janeiro"
+    
+    if( mes == "janeiro" || mes == "dezembro"){
+    console.log("Você é de Janeiro, estou chegando perto?")
+    }else
+    {
+    console.log("Ainda não descobri seu aniversário mas vou conseguir")
+    }
+    }
+    
+    
